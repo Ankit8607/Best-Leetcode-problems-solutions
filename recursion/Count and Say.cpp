d52fda9fc776248cd13ac889d9c4f024ef60202c
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-08-29 16:51:31 +0530

    Create Count and Say.cpp

---
+++ recursion/Count and Say.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/count-and-say/
+
+/*
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+Constraints:
+
+1 <= n <= 30
+
+*/
+
+
+class Solution {
+public:
+    string ans="1";
+    string countAndSay(int n) {
+        if(n==1) return ans;
+        string tmp;
+        int i=0;
+        while(i<ans.size()){
+            int cnt=1;
+            while(i+1<ans.size() and ans[i]==ans[i+1]) cnt++,i++;
+            tmp+=to_string(cnt)+ans[i];
+            i++;
+        }
+        ans=tmp;
+        return countAndSay(n-1);
+    }
+};
