dbd3b2356157663044754215329e7cc655f6c822
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2022-12-26 16:32:37 +0530

    Create Last Stone Weight II.cpp

---
+++ dp/Last Stone Weight II.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/last-stone-weight-ii/description/
+
+class Solution {
+public:
+    int dp[31][3001],sum=0;
+
+    int rec(int level,int pos,vector<int> &stones){
+        if(level==stones.size()) return ((pos-(sum-pos))>=0?2*pos-sum:1e9);
+
+        if(dp[level][pos]!=-1) return dp[level][pos];
+
+        return dp[level][pos]=min(rec(level+1,pos,stones),rec(level+1,pos+stones[level],stones));
+    }
+    int lastStoneWeightII(vector<int>& stones) {
+        memset(dp,-1,sizeof(dp));
+        for(int &i:stones) sum+=i;
+        return rec(0,0,stones);
+    }
+};
