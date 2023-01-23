ac9f6ea0066503d5755e3ef396754f0aa2a7708b
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-01-23 11:37:31 +0530

    Create Find the Town Judge.cpp

---
+++ array/Find the Town Judge.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>he(n+1,0),tr(n+1,0);
+        for(auto &i:trust){
+            he[i[0]]++;
+            tr[i[1]]++;
+        }
+        int ans=-1;
+        for(int i=1;i<=n;i++){
+            if(he[i]==0 and tr[i]==n-1){
+                if(ans!=-1) return -1;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
