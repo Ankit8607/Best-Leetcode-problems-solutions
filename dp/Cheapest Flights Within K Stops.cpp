8d9e408924726fc626e3e58ddf75951eca46ed3d
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-01-26 16:21:49 +0530

    Create Cheapest Flights Within K Stops.cpp

---
+++ dp/Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+class Solution {
+public:
+    int dp[101][101];
+
+    int rec(int src,int dst,int k,vector<vector<pair<int,int>>>&g){
+        if(k<0) return 1e9;
+
+        if(src==dst) return 0;
+
+        if(dp[src][k]!=-1) return dp[src][k];
+
+        int ans=1e9;
+        for(auto &i:g[src]){
+            ans=min(ans,i.second+rec(i.first,dst,k-1,g));
+        }
+
+        return dp[src][k]=ans;
+    }
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> g(n);
+        memset(dp,-1,sizeof(dp));
+        for(auto &i:flights) g[i[0]].push_back({i[1],i[2]});
+        int ans=rec(src,dst,k+1,g);
+        return ans>=1e9?-1:ans;
+    }
+};
