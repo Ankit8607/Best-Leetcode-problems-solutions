ceed0b9c62f2a58e508c1bfe046d4adea09c8c2f
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-01-30 11:47:26 +0530

    Create N-th Tribonacci Number.cpp

---
+++ maths/N-th Tribonacci Number.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n<2) return n;
+        if(n==2) return 1;
+        int one=0,two=1,three=1;
+        for(int i=4;i<=n;i++){
+            int tmp=three;
+            three=one+two+three;
+            one=two;
+            two=tmp;
+        }
+        return one + two + three;
+    }
+};
