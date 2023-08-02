2e7627829f7f01fce36f1602da40257df5e83aa5
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-08-02 21:59:15 +0530

    Create K-Similar Strings.cpp

---
+++ recursion/K-Similar Strings.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/k-similar-strings/solutions/140299/c-6ms-solution/
+
+class Solution {
+public:
+    int kSimilarity(string A, string B) {
+      for (int i=0; i<A.size(); i++) {
+        if (A[i]==B[i]) continue;
+        vector<int> matches;
+        for (int j=i+1;j<A.size();j++) { 
+          if (A[j]==B[i] && A[j]!=B[j]) {
+            matches.push_back(j);
+            if (A[i]==B[j]) {
+              swap(A[i],A[j]);
+              return 1+kSimilarity(A.substr(i+1),B.substr(i+1));
+            }
+          }
+        }
+        int best=A.size()-1;
+        for (int j: matches) {
+            swap(A[i],A[j]);
+            best = min(best, 1+kSimilarity(A.substr(i+1),B.substr(i+1)));
+            swap(A[i],A[j]);
+        }
+        return best;
+      }
+      return 0;
+    }
+};
