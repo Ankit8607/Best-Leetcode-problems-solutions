6d39847f2838de53b0b91238b500314b9cf44203
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-20 17:03:54 +0530

    Update Maximum Profit in Job Scheduling.cpp

--- sorting/Maximum Profit in Job Scheduling.cpp
+++ sorting/Maximum Profit in Job Scheduling.cpp
@@ -2,7 +2,7 @@
 
 class Solution{
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)   {
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit){
         int n = startTime.size();
         vector<vector<int>> jobs;
         for (int i = 0; i < n; ++i) {
