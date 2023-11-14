0e727bf9a79f79d949eb5386ba8952427fb9fd54
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-11-14 08:39:08 +0530

    Update Unique Length-3 Palindromic Subsequences.cpp

--- prefix-sum/Unique Length-3 Palindromic Subsequences.cpp
+++ prefix-sum/Unique Length-3 Palindromic Subsequences.cpp
@@ -9,7 +9,7 @@ public:
         memset(arr, 0, sizeof(arr));
         memset(st, -1, sizeof(st));
         memset(end, -1, sizeof(end));
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ 
             int t=s[i]-'a';
             if(st[t]==-1) st[t]=i;
             else end[t]=i;
