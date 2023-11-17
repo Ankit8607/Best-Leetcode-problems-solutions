837f6bf4bf757746f4bc3c7d2f3478031ca014c3
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-11-17 09:45:12 +0530

    Update Minimize Maximum Pair Sum in Array.cpp

--- two pointers/Minimize Maximum Pair Sum in Array.cpp
+++ two pointers/Minimize Maximum Pair Sum in Array.cpp
@@ -5,7 +5,7 @@ public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int ans =0;
-        int l=0,r=nums.size()-1;
+        int l=0,r=nums.size()-1; 
         while(l<r){
             ans=max(ans,nums[r]+nums[l]);
             l++; r--;
