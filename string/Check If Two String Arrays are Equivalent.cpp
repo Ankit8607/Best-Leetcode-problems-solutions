98f877c8338e19fbb50cf7bb55a5faba68d1a554
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-12-01 09:59:50 +0530

    Create Check If Two String Arrays are Equivalent.cpp

---
+++ string/Check If Two String Arrays are Equivalent.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/?envType=daily-question&envId=2023-12-01
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string a,b;
+        for(auto i:word1) a+=i;
+        for(auto i:word2) b+=i;
+        return a==b;
+    }
+};
