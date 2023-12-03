561ac042561e1f835b77be03f3f9dfd789803a9b
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-12-03 20:21:44 +0530

    Create Minimum Time Visiting All Points.cpp

---
+++ maths/Minimum Time Visiting All Points.cpp
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/minimum-time-visiting-all-points/description/?envType=daily-question&envId=2023-12-03
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans=0;
+        for(int i=1;i<points.size();i++) ans+=max(abs(points[i][1]-points[i-1][1]),abs(points[i][0]-points[i-1][0]));
+        return ans;
+    }
+};
