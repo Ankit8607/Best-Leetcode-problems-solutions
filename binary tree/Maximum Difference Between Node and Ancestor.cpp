0ccf1a603e94f9f0733124ca6a0686118e52eea4
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-01-11 09:05:51 +0530

    Update Maximum Difference Between Node and Ancestor.cpp

--- binary tree/Maximum Difference Between Node and Ancestor.cpp
+++ binary tree/Maximum Difference Between Node and Ancestor.cpp
@@ -3,7 +3,7 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
+ *     int val; 
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
