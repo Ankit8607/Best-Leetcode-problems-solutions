cc14a194bd0e0a66640adc96d87dc4e422108209
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-03-09 07:18:35 +0530

    Update Minimum Common Value.cpp

--- two pointers/Minimum Common Value.cpp
+++ two pointers/Minimum Common Value.cpp
@@ -6,7 +6,7 @@ public:
         int i=0,j=0,n=nums1.size(),m=nums2.size();
         while(i<n and j<m){  
             if(nums1[i]==nums2[j]) return nums1[i];  
-            if(nums1[i]>nums2[j]) j++; 
+            if(nums1[i]>nums2[j]) j++;  
             else i++;
         }
         return -1; 
