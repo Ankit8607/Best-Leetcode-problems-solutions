0b5a7cc5f60dbf2c7a22ea7a75f54bf7e6d328a8
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-03-28 00:04:39 +0530

    Update Subarray Product Less Than K.cpp

--- sliding window/Subarray Product Less Than K.cpp
+++ sliding window/Subarray Product Less Than K.cpp
@@ -19,3 +19,52 @@ public:
         return ans;
     }
 };
+
+
+
+// In python 
+
+// class Solution:
+//     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+//         l,r,ans=0,0,0
+//         n,mul=len(nums),1
+//         while l<n:
+//             while r<n and nums[r]*mul<k:
+//                 mul=mul*nums[r]
+//                 r+=1
+//             if mul<k:
+//                 ans+=r-l
+//             if r>l: 
+//                 mul/=nums[l]
+//             l+=1
+//             if l>r: 
+//                 r=l
+
+//         return ans
+
+
+
+
+// In Javascript
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {number}
+//  */
+// var numSubarrayProductLessThanK = function(nums, k) {
+//     var l=0,r=0,ans=0,n=nums.length,mul=1;
+//     while(l<n){
+//         while(r<n && nums[r]*mul<k){
+//             mul=mul*nums[r];
+//             r++;
+//         }
+//         if(mul<k){
+//             ans+=r-l;
+//         }
+//         if(r>l) mul/=nums[l];
+//         l++;
+//         if(l>r) r=l;
+//     }
+//     return ans;
+// };
