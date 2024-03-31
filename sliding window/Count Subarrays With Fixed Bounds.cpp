6f936fab975cbeda136e57d84abdfeaa39ede48b
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-03-31 08:56:17 +0530

    Update Count Subarrays With Fixed Bounds.cpp

--- sliding window/Count Subarrays With Fixed Bounds.cpp
+++ sliding window/Count Subarrays With Fixed Bounds.cpp
@@ -16,3 +16,45 @@ public:
         return ans;
     }
 };
+
+
+// In python 
+
+// class Solution:
+//     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+//         ans,mn,mx,last=0,-1,-1,-1
+//         for i in range(0,len(nums)):
+//             if nums[i]<minK or nums[i]>maxK:
+//                 mn=-1
+//                 mx=-1
+//                 last=i 
+//             if nums[i]==minK:
+//                 mn=i 
+//             if nums[i]==maxK:
+//                 mx=i 
+//             if mn!=-1 and mx!=-1:
+//                 ans+=min(mx,mn)-last 
+//         return ans 
+
+
+// In javascript
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} minK
+//  * @param {number} maxK
+//  * @return {number}
+//  */
+// var countSubarrays = function(nums, minK, maxK) {
+//     let ans=0;
+//     let mn=-1,mx=-1,last=-1;
+//     for(let i=0;i<nums.length;i++){
+//         if(nums[i]<minK || nums[i]>maxK){
+//             mn=-1; mx=-1; last=i;
+//         }
+//         if(nums[i]==minK) mn=i;
+//         if(nums[i]==maxK) mx=i;
+//         if(mn!=-1 && mx!=-1) ans+=Math.min(mx,mn)-last;
+//     }
+//     return ans;
+// };
