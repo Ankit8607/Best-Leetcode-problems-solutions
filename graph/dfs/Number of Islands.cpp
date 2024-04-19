4970385ea18090199f25cc59dae96bd745cc29a6
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-04-19 21:59:49 +0530

    Update Number of Islands.cpp

--- graph/dfs/Number of Islands.cpp
+++ graph/dfs/Number of Islands.cpp
@@ -28,3 +28,58 @@ public:
         return ans;
     }
 };
+
+
+
+// In python
+
+// class Solution:
+//     def numIslands(self, grid: List[List[str]]) -> int:
+//         def dfs(i,j,g,vis):
+//             if i>=len(g) or j>=len(g[0]) or i<0 or j<0 or g[i][j]=='0' or vis[i][j]: return 
+
+//             vis[i][j]=1
+//             dfs(i+1,j,g,vis)
+//             dfs(i-1,j,g,vis)
+//             dfs(i,j+1,g,vis)
+//             dfs(i,j-1,g,vis)
+//         n=len(grid)
+//         m=len(grid[0])
+//         vis= [[0 for i in range(m)] for j in range(n)]
+//         ans=0
+//         for i in range(n):
+//             for j in range(m): 
+//                 if grid[i][j]=='1' and not vis[i][j]: 
+//                     dfs(i,j,grid,vis)
+//                     ans+=1
+//         return ans
+    
+
+
+// In javascript
+
+// /**
+//  * @param {character[][]} grid
+//  * @return {number}
+//  */
+// var numIslands = function(grid) {
+//     let n=grid.length,m=grid[0].length;
+//     let ans=0;
+//     for(let i=0;i<n;i++){
+//         for(let j=0;j<m;j++){ 
+//             if(grid[i][j]=='1'){ 
+//                 dfs(i,j,grid);
+//                 ans++;
+//             }
+//         }
+//     }
+//     return ans;
+// };
+// var dfs =  (i,j,g)=>{
+//     if(i>=g.length || j>=g[0].length || i<0 || j<0 || g[i][j]=='0') return ;
+//     g[i][j]='0';
+//     dfs(i+1,j,g);
+//     dfs(i-1,j,g);
+//     dfs(i,j+1,g);
+//     dfs(i,j-1,g);
+// }
