7514b55b94174b90f0141b774058e32876aff5e5
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-04-24 23:34:59 +0530

    Update N-th Tribonacci Number.cpp

--- maths/N-th Tribonacci Number.cpp
+++ maths/N-th Tribonacci Number.cpp
@@ -6,7 +6,7 @@ public:
         if(n<2) return n;
         if(n==2) return 1;
         int one=0,two=1,three=1;
-        for(int i=4;i<=n;i++){
+        for(int i=4;i<=n;i++){ 
             int tmp=three;
             three=one+two+three;
             one=two;
