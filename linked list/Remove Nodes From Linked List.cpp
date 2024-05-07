f20b1bc5882ac2c1b935784a5c9781b0ee7e0596
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-05-07 21:39:30 +0530

    Update Remove Nodes From Linked List.cpp

--- linked list/Remove Nodes From Linked List.cpp
+++ linked list/Remove Nodes From Linked List.cpp
@@ -28,3 +28,39 @@ public:
         return rec(head,mx);
     }
 };
+
+
+// In python
+
+// # Definition for singly-linked list.
+// # class ListNode:
+// #     def __init__(self, val=0, next=None):
+// #         self.val = val
+// #         self.next = next
+// class Solution:
+//   def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+//     if not head:
+//       return None
+//     head.next = self.removeNodes(head.next)
+//     return head.next if head.next and head.val < head.next.val else head
+
+
+// In javascript
+
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+// var removeNodes = function(head) {
+//     if(!head)
+//       return null;
+//     head.next = removeNodes(head.next)
+//     return (head.next && (head.val < head.next.val))? head.next:head;
+// };
