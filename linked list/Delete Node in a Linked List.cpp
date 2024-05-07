9c5be6473f911f3a744aecf087864dbfcf68f2e0
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-05-07 22:07:47 +0530

    Update Delete Node in a Linked List.cpp\

--- linked list/Delete Node in a Linked List.cpp
+++ linked list/Delete Node in a Linked List.cpp
@@ -19,3 +19,42 @@ public:
        head->next=NULL;
     }
 };
+
+// In python
+
+// # Definition for singly-linked list.
+// # class ListNode:
+// #     def __init__(self, x):
+// #         self.val = x
+// #         self.next = None
+
+// class Solution:
+//     def deleteNode(self, head):
+//         while head.next.next:
+//             head.val=head.next.val
+//             head=head.next
+//         head.val=head.next.val
+//         head.next=None
+
+
+// In javascript
+
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+// /**
+//  * @param {ListNode} node
+//  * @return {void} Do not return anything, modify node in-place instead.
+//  */
+// var deleteNode = function(head) {
+//     while(head.next.next){
+//         head.val=head.next.val;
+//         head=head.next;
+//     }
+//     head.val=head.next.val;
+//     head.next=null;
+// };
