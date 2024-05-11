72f21e37c571e1afad0a54367109762625337a94
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-05-11 07:05:02 +0530

    Update K-th Smallest Prime Fraction.cpp

--- binary search/K-th Smallest Prime Fraction.cpp
+++ binary search/K-th Smallest Prime Fraction.cpp
@@ -23,3 +23,52 @@ public:
         return res;
     }
 };
+
+
+In python
+
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        res = [0,1]
+        l,r=0.0,1.0
+        n = len(arr)
+        while l<=r:
+            mid=(l+r)/2.0
+            j,total=1,0
+            res[0]=0
+            res[1]=1
+            for i in range(n):
+                while j<n and arr[i]>=arr[j]*mid: j+=1
+                total+=n-j
+                if j<n and res[0]*1.0/res[1] < arr[i]*1.0/arr[j]: res=[arr[i],arr[j]]
+            if total==k: break
+            if total<k: l=mid
+            else: r=mid
+        return res
+
+In javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var kthSmallestPrimeFraction = function(arr, k) {
+    res=[0,1]; 
+    let l=0.0,r=1.0;
+    let n = arr.length;
+    while(l<=r){
+        let mid=(l+r)/2;
+        let j=1,total=0;
+        res[0]=0,res[1]=1;
+        for(let i=0;i<n;i++){
+            while(j<n && arr[i]>=arr[j]*mid) j++;
+            total+=n-j;
+            if(j<n && res[0]*1.0/res[1] < arr[i]*1.0/arr[j]) res=[arr[i],arr[j]];
+        }
+        if(total==k) break;
+        if(total<k) l=mid;
+        else r=mid;
+    }
+    return res;
+};
