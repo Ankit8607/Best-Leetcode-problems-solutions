4b555a25bdcf9d1ba2b27addd96e40a4ce31a218
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2024-06-19 12:30:01 +0530

    Update Minimum Number of Days to Make m Bouquets.cpp

--- binary search/Minimum Number of Days to Make m Bouquets.cpp
+++ binary search/Minimum Number of Days to Make m Bouquets.cpp
@@ -25,3 +25,57 @@ public:
         return ans;
     }
 };
+
+
+// In python 
+
+// class Solution:
+//     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+//         if m*k > len(bloomDay): return -1
+//         ans,l,r=0,0,1e9
+//         while l<=r:
+//             mid=int((l+r)/2)
+//             cnt,streak=0,0
+//             for i in range(len(bloomDay)):
+//                 if bloomDay[i]<=mid: streak+=1
+//                 else: streak=0
+//                 if streak==k:
+//                     cnt+=1 
+//                     streak=0
+//             if cnt>=m:
+//                 ans=mid
+//                 r=mid-1
+//             else: l=mid+1
+//         return int(ans)
+
+// In javascript
+
+// /**
+//  * @param {number[]} bloomDay
+//  * @param {number} m
+//  * @param {number} k
+//  * @return {number}
+//  */
+// var minDays = function(bloomDay, m, k) {
+//     if(m*k>bloomDay.length) return -1;
+//     let ans=0, l=0, r=1e9;
+//     while(l<=r){
+//         let mid=parseInt((l+r)/2);
+//         console.log(mid);
+//         let cnt=0,streak=0;
+//         for(let i=0;i<bloomDay.length;i++){
+//             if(bloomDay[i]<=mid) streak++;
+//             else streak=0;
+//             if(streak==k){
+//                 cnt++; 
+//                 streak=0;
+//             }
+//         }
+//         if(cnt>=m){
+//             ans=mid;
+//             r=mid-1;
+//         }
+//         else l=mid+1;
+//     }
+//     return ans;
+// };
